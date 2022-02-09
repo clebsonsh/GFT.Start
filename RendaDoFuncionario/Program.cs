67043452318d6d82bd128ec9cb3f6cdde386a9d8
clebson moura <clebson.sh@gmail.com>
2022-02-09 10:27:54 -0300

    Setup inicial do projeto

---
+++ RendaDoFuncionario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RendaDoFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
